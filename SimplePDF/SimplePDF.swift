cebe183c6e16fe5f0f1b54d98a755b0ee56e10ab
Nick Matelli <nmatelli@enova.com>
2017-07-12 15:16:04 -0500

    Updated syntax to support Swift 4

--- SimplePDF/SimplePDF.swift
+++ SimplePDF/SimplePDF.swift
@@ -103,9 +103,9 @@ open class SimplePDF {
             paragraphStyle.alignment = .right
         }
         
-        let attributes: [String:NSObject] = [
-            NSFontAttributeName: font,
-            NSParagraphStyleAttributeName: paragraphStyle
+        let attributes: [NSAttributedStringKey:NSObject] = [
+            NSAttributedStringKey.font: font,
+            NSAttributedStringKey.paragraphStyle: paragraphStyle
         ]
         let attributedText = NSAttributedString(string: text, attributes: attributes)
         
@@ -309,14 +309,14 @@ open class SimplePDF {
         
         let skew = 0.0
         
-        let attributes: [String: AnyObject] = [
-            NSForegroundColorAttributeName: fieldColor,
-            NSParagraphStyleAttributeName: paraStyle,
-            NSObliquenessAttributeName: skew as AnyObject,
-            NSFontAttributeName: font
+        let attributes: [NSAttributedStringKey: AnyObject] = [
+            NSAttributedStringKey.foregroundColor: fieldColor,
+            NSAttributedStringKey.paragraphStyle: paraStyle,
+            NSAttributedStringKey.obliqueness: skew as AnyObject,
+            NSAttributedStringKey.font: font
         ]
         
-        let size = text.size(attributes: attributes)
+        let size = text.size(withAttributes: attributes)
         
         let x = (rect.size.width - size.width)/2
         let y = (rect.size.height - size.height)/2
